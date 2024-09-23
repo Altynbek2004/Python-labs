66eb75140c8b4871fc4678e7820b3f7076a380ff
Altynbek2004 <altynaleuetdinov@gmail.com>
2024-09-23 16:46:56 +0500

    "Second git"

---
+++ Lab21.py
@@ -0,0 +1,59 @@
+print("4","8","15","16","23","42")
+
+print(" 4\n","8\n","15\n","16\n","23\n","42")
+
+x=int(input("Введите число:"))
+
+print(x)
+print(x+1)
+print(x+2)
+
+y=int(input('Введите 3 число:'))
+z=int(input())
+a=int(input())
+
+print(y+z+a)
+
+
+b=int(input('Введите число для cчисления куба:'))
+
+c=6*b*b
+d=b*b*b
+
+
+
+print("Volume=", d)
+print("Total surface area =", c)
+
+
+
+e=int(input())
+f=int(input())
+
+g=e//f
+h=e%f
+
+print("В корзине=", h)
+print("Каждому=", g)
+
+i=int(input("Введите 4 танбалы сан:"))
+
+j=i//1000
+i=i-j*1000
+k=i//100
+i=i-k*100
+l=i//10
+i=i-l*10
+
+print("The digit in the thousands position ",j)
+print("The number in the hundreds position is  ",k)
+print("The digit in the tens position",l)
+print("The digit in the position of units  ",i)
+
+
+
+m=int(input("сан:"))
+
+n=round(m/2)
+
+print(n)
