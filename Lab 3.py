46f9b9ddc800b4a8af8738d5d2652aa225d9d621
Altynbek2004 <altynaleuetdinov@gmail.com>
2024-09-28 16:01:36 +0500

    Fix

---
+++ Lab 3.py
@@ -0,0 +1,109 @@
+# 1
+# x=int(input("Введите число: "))
+#
+# y=x<<1
+#
+# if y==0 :
+#     print(f"Предупреждение: {y}")
+# else:
+#     print(f"Число: {y}")
+
+
+# 2
+# x=float(input("Please enter the first number:  "))
+# y=float(input("Please enter the second number:  "))
+# z=str(input("Please choose the operation (+, -, *, /)  "))
+#
+# if z=="+":
+#     print(f"{x}+{y}={x+y}")
+# if z=="-":
+#     print(f"{x}-{y}={x-y}")
+# if z=="*":
+#     print(f"{x}*{y}={x*y}")
+# if z=="/" and y!=0:
+#     print(f"{x}/{y}={x/y}")
+# if z=="/" and y==0:
+#     print("Санды 0-ге бөлуге болмайды")
+
+# 3
+
+# i=int(input("4 таңбалы санды енгізіңіз:"))
+#
+# j=i//1000
+# i=i-j*1000
+# k=i//100
+# i=i-k*100
+# l=i//10
+# i=i-l*10
+#
+# if j+i==k-l:
+#     print("True")
+# else :
+#     print("False")
+
+# 4
+
+# i=int(input("Жасын енгізіңіз:"))
+#
+# if i>18:
+#     print("True")
+# else :
+#     print("False")
+
+
+# 5
+
+# x=int(input("1 san енгізіңіз:"))
+# y=int(input("2 san енгізіңіз:"))
+# z=int(input("3 san енгізіңіз:"))
+#
+# if y-x==z-y:
+#     print("True")
+# else :
+#     print("False")
+
+
+# 6
+# x=int(input("Enter a number: "))
+# y=int(input("Enter a number: "))
+# x_2=int(input("Enter a number: "))
+# y_2=int(input("Enter a number: "))
+#
+# if x == x_2 or y == y_2:
+#     print("YES")
+# else:
+#     print("NO")
+
+
+
+# 7
+
+x=float(input("1 san енгізіңіз:"))
+y=float(input("2 san енгізіңіз:"))
+z=float(input("3 san енгізіңіз:"))
+d=float(input("3 san енгізіңіз:"))
+#
+# a=x+y+z/3
+#
+# if x>a:
+#     print(x)
+# if y>a:
+#     print(y)
+# if z>a:
+#     print(z)
+
+
+
+# 8
+# x=int(input("1 san енгізіңіз:"))
+# y=int(input("2 san енгізіңіз:"))
+# z=int(input("3 san енгізіңіз:"))
+#
+# if x>y and y<z:
+#     print(y)
+# if x>z and z<y:
+#     print(z)
+# if z>x and x<y:
+#     print(x)
+
+
