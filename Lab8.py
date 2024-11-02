4a600859077134d299f7886ab390dfd657e10782
Altynbek2004 <altynaleuetdinov@gmail.com>
2024-11-02 10:52:55 +0500

    Task:all tasks was done

--- Lab8.py
+++ Lab8.py
@@ -23,25 +23,64 @@ from itertools import count
 # print('Символдар:',list_sym)
 
 # 2.1
-# name=input('Student name - ')
-# assignments =input('scores for assignments = ').split(',')
-# labs=input('labs= ').split(',')
-# tests =input('tests= ').split(',')
-#
-# student={
-# 'name':name,
-# 'assignment':assignments,
-# 'test':tests,
-# 'lab':labs
-# }
-#
-# print(student)
+name=input('Student name - ')
+assignments =input('scores for assignments = ').split(',')
+labs=input('labs= ').split(',')
+tests =input('tests= ').split(',')
+
+student={
+'name':name,
+'assignment':assignments,
+'test':tests,
+'lab':labs
+}
+
+print(student)
 #
 # # 2.2
-# count=len(student['assignment'])+len(student['test'])+len(student['lab'])
+count_submission=len(student['assignment'])+len(student['test'])+len(student['lab'])
+
+submission_rate={
+    student['name']:count_submission
+}
+
+print(submission_rate)
+
+# 2.3
+
+# def average(tasks):
+#     all_final=0
+#     for task in tasks:
+#         all_final+=float(task)
+#     return all_final/len(tasks) if tasks else 0
 #
-# task={
-#     student['name']:count
+#
+#
+#
+# if submission_rate[student['name']]>=4:
+#     final_grade=0.3*average(student['assignment'])+0.5*average(student['lab'])+0.2*average(student['test'])
+#     student['final_grade']= final_grade
+#     print(student)
+# else:
+#     student['final_grade']=0
+#     print(student)
+
+# 2.4
+
+# student2={
+#     'name': 'Kevin',
+#     'assignment': [82, 30],
+#     'test': [],
+#     'lab': [78.2]
 # }
 #
-# print(task)
+# student_name1=student['name']
+# student_name2=student2['name']
+# del student['name']
+# del student2['name']
+# students={
+#     student_name1:student,
+#     student_name2:student2
+# }
+#
+# print(students)
